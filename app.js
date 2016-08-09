468b10beb2a46f7c9377d6000f7ef2d7dabe164a
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-08-08 22:19:57 -0500

    Landing page complete.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //set the port to server port or 3000 for local testing
 
+//serve static assets from the public folder
+app.use('/assets', express.static(__dirname + '/public'));
+
+//main page route
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html', {root: __dirname + '/public/pages'});
+});
+
+//route for handling timestamp requests
+app.get('/*', function (req, res){
+
 });
 
 app.listen(port);
